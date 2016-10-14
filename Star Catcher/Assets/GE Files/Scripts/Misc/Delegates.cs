8742bf18e14706effa89e1d2314a991944e48cc3
Niels Dunn <nielsdunn@gmail.com>
2016-10-14 13:57:18 -0600

    Organized Scripts into Folders

---
+++ Star Catcher/Assets/GE Files/Scripts/Misc/Delegates.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Delegates : MonoBehaviour {
+
+	Action<Delegates> Move;
+	Action Idle;
+	Action Jump;
+
+	int speed = 200;
+
+
+	void MoveHandler (Delegates obj)
+	{
+		print("Moving at " + obj.speed + " mph.");
+		Move = null;
+		Idle = IdleHandler;
+	}
+
+	void IdleHandler ()
+	{
+		print("Idling");
+		Idle = null;
+		Jump = JumpHandler;
+
+	}
+
+	void JumpHandler ()
+	{
+		print("Jumping");
+		Jump = null;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Move = MoveHandler;
+
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	if(Move != null)
+		Move(this);
+	if(Idle != null)
+		Idle();
+	if(Jump != null)
+		Jump();
+	}
+}

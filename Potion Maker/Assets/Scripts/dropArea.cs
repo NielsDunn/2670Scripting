8d5e49b6dadecedadd0d6ff5adac5ef19fd3e906
Niels Dunn <nielsdunn@gmail.com>
2016-11-14 13:10:50 -0700

    Started new Script in Potion Maker
    Trying out variables and stuff.

---
+++ Potion Maker/Assets/Scripts/dropArea.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dropArea : MonoBehaviour {
+
+	public bool active = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter()
+	{
+		active = true;
+	}
+
+	void OnMouseUp()
+	{
+		if(active == true) 
+		{
+			print("Ingredient Added");
+		}
+	}
+}

147e5bbef1b7a8d0577a4015e21f2b7dfbe7e87b
Niels Dunn <nielsdunn@gmail.com>
2016-11-16 11:44:57 -0700

    Added more arrays to script

--- LearnToCode/Assets/ListTimeBaby.cs
+++ LearnToCode/Assets/ListTimeBaby.cs
@@ -3,17 +3,25 @@ using System.Collections;
 
 public class ListTimeBaby : MonoBehaviour {
 
-	string playerone = "JonnyKiller25";
-	string playertwo = "BamBamBunnyLover18";
-	string playerthree = "Devslopes";
-	string playerfour = "PonyL0ver";
+	public GameObject cubePrefab;
+
+	GameObject[] cubes = new GameObject[5];
+	public Material[] colors = new Material[5];
+
+	float spacer = 0.5f;
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Welcome: " + playerone);
-		Debug.Log ("Welcome: " + playertwo);
-		Debug.Log ("Welcome: " + playerthree);
-		Debug.Log ("Welcome: " + playerfour);
+
+
+		for (int i = 0; i < cubes.Length; i++)
+		{
+			GameObject cube = Instantiate (cubePrefab);
+			cubes [i] = cube;
+			cube.transform.position = new Vector3 (i + (spacer * i), cube.transform.position.y);
+		
+		}
+
 	}
 	
 	// Update is called once per frame

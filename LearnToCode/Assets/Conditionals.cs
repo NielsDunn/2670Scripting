55d24bf66fbcae29a420b1b0390f3db4efcbda1e
Niels Dunn <nielsdunn@gmail.com>
2016-11-16 09:08:25 -0700

    More Practice Work

---
+++ LearnToCode/Assets/Conditionals.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Conditionals : MonoBehaviour {
+
+	int playerOneTowersRemaining = 2;
+	int playerTwoTowersRemaining = 2;
+	bool playerOneMainTowerDestroyed = false;
+	bool playerTwoMainTowerDestroyed = false;
+	float timer = 200;
+
+	// Use this for initialization
+	void Start () {
+
+		if (playerOneMainTowerDestroyed || playerTwoMainTowerDestroyed)
+		{
+			if (playerOneMainTowerDestroyed) 
+			{
+				Debug.Log ("Player Two Wins!");
+			} else 
+			{
+				Debug.Log ("Player One Wins!");
+			}
+		} else if (timer <= 0)
+		{
+			if (playerOneTowersRemaining < playerTwoTowersRemaining)
+			{
+				Debug.Log ("Player Two Wins!");
+			} else if (playerTwoTowersRemaining < playerOneTowersRemaining)
+			{
+				Debug.Log ("Player One Wins!");
+			} else 
+			{
+				Debug.Log ("The Game was a Draw!");
+			}
+		}
+
+		if (true == false || false != true && 1 == 1)
+		{
+		Debug.Log ("Did we get here?");
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ LearnToCode/Assets/ListTimeBaby.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ListTimeBaby : MonoBehaviour {
+
+	string playerone = "JonnyKiller25";
+	string playertwo = "BamBamBunnyLover18";
+	string playerthree = "Devslopes";
+	string playerfour = "PonyL0ver";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Welcome: " + playerone);
+		Debug.Log ("Welcome: " + playertwo);
+		Debug.Log ("Welcome: " + playerthree);
+		Debug.Log ("Welcome: " + playerfour);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

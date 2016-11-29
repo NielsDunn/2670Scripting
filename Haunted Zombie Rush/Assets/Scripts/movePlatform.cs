37752ed75348b03c811443fcbcd2f3e2b94ce36d
Niels Dunn <nielsdunn@gmail.com>
2016-11-28 22:58:28 -0700

    Added Repeating Platforms & Obstacle to HZR
    Platforms repeat endlessly, and obstacle moves up and down.

---
+++ Haunted Zombie Rush/Assets/Scripts/movePlatform.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlatform : MonoBehaviour 
+{
+
+	[SerializeField] private float objectSpeed = 1;
+	[SerializeField] private float resetPosition = -0.4f;
+	[SerializeField] private float startPosition = 87.6f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+		if (transform.localPosition.x <= resetPosition)
+		{
+			Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+			transform.position = newPos;
+		}
+	}
+}

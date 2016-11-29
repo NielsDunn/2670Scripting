927b03d17f48cab6064d1a47108975fcdd862463
Niels Dunn <nielsdunn@gmail.com>
2016-11-29 07:26:45 -0700

    HZR - Added Player Mechanics & Prepped for SFX
    Character jumps & has audio source ready for sfx clips

--- Haunted Zombie Rush/Assets/Scripts/Player.cs
+++ Haunted Zombie Rush/Assets/Scripts/Player.cs
@@ -3,20 +3,43 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	Animator anim;
+	[SerializeField] private float forceAmount = 50f;
+	[SerializeField] private AudioClip sfxJump;
+
+	private Animator anim;
+	private Rigidbody rigidBody;
+	private bool jump = false;
+	private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
+		rigidBody = GetComponent<Rigidbody> ();
+		audioSource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 
 		if (Input.GetMouseButtonDown (0)) 
 		{
 			anim.Play("Jump");
+			audioSource.PlayOneShot(sfxJump);
+			rigidBody.useGravity = true;
+			jump = true;
 		}
 	
+	}
+
+	void FixedUpdate()
+	{
+		if (jump == true) 
+		{
+			jump = false;
+			rigidBody.velocity = new Vector2 (0,0);
+			rigidBody.AddForce(new Vector2(0, forceAmount), ForceMode.Impulse);
+		}
+
 	}
 }

1c5cba2042b5515593252b1bf2dd8034fafc6eda
Niels Dunn <nielsdunn@gmail.com>
2016-11-28 20:20:50 -0700

    Changed Potion Maker File Locations
    Moved stuff around and organized things

---
+++ Potion Maker/Assets/Scripts/DragObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObject : MonoBehaviour {
+
+	Vector3 screenPoint;
+	Vector3 offset;
+	Vector3 clickPosition;
+	Vector3 movePosition;
+
+	void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	void OnMouseDrag()
+	{
+		clickPosition = Input.mousePosition;
+		movePosition = Camera.main.ScreenToWorldPoint(clickPosition) + offset;
+		transform.position = movePosition;
+	}
+}

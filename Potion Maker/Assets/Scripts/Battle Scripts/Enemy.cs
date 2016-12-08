6caa07f761ad1305191548da608566dece42ff35
Niels Dunn <nielsdunn@gmail.com>
2016-12-08 10:52:17 -0700

    PM - Enemy Movement Started
    Enemy moves left to right, but not smoothly

--- Potion Maker/Assets/Scripts/Battle Scripts/Enemy.cs
+++ Potion Maker/Assets/Scripts/Battle Scripts/Enemy.cs
@@ -7,18 +7,57 @@ public class Enemy : MonoBehaviour, IDamageable {
 
 	public int respawnTime = 5;
 
+	public float smoothing = 10f;
+    public Transform targetLeft;
+    public Transform targetRight;
+
 	// Use this for initialization
 	void Start () {
-	
+
+		StartCoroutine(moveLeft(targetLeft));
+
 	}
 
 	public void damageTaken ()
 	{
-		throw new System.NotImplementedException ();
+		
 	}
 
 	public void Respawn ()
 	{
 		throw new System.NotImplementedException ();
 	}
+
+	IEnumerator moveLeft (Transform targetLeft)
+    {
+        while(Vector3.Distance(transform.position, targetLeft.position) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetLeft.position, smoothing * Time.deltaTime);
+            
+            yield return null;
+        }
+        
+        print("Reached the target.");
+        
+        yield return new WaitForSeconds(1f);
+        
+        StartCoroutine(moveRight(targetRight));
+    }
+
+	IEnumerator moveRight (Transform targetRight)
+    {
+        while(Vector3.Distance(transform.position, targetRight.position) > 0.05f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetRight.position, smoothing * Time.deltaTime);
+            
+            yield return null;
+        }
+        
+        print("Reached the target.");
+        
+        yield return new WaitForSeconds(1f);
+        
+        StartCoroutine(moveLeft(targetLeft));
+    }
+
 }
---
+++ Potion Maker/Assets/Scripts/Battle Scripts/Potion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Potion : MonoBehaviour, IReuseable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void Refill ()
+	{
+		throw new System.NotImplementedException ();
+	}
+
+}

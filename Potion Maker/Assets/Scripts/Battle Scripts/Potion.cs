9b36f0f3259c62c8343ce24772ac44c83e48daf7
Niels Dunn <nielsdunn@gmail.com>
2016-12-09 10:07:15 -0700

    PM - Started programming attack
    "Potion" returns to player after it hits enemy.

--- Potion Maker/Assets/Scripts/Battle Scripts/Potion.cs
+++ Potion Maker/Assets/Scripts/Battle Scripts/Potion.cs
@@ -2,10 +2,21 @@
 using System.Collections;
 
 public class Potion : MonoBehaviour, IReuseable {
+	
+	public float upwardForce = 3f;
+	public float forwardForce = 1f;
+	private Transform startPosition;
+
+	private Rigidbody rigidBody;
 
 	// Use this for initialization
 	void Start () {
-	
+		rigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void OnMouseUp()
+	{
+		rigidBody.AddForce(new Vector3(0, upwardForce, forwardForce), ForceMode.Impulse);
 	}
 
 	public void Refill ()
@@ -13,4 +24,8 @@ public class Potion : MonoBehaviour, IReuseable {
 		throw new System.NotImplementedException ();
 	}
 
+	void OnTriggerEnter ()
+	{
+
+	}
 }

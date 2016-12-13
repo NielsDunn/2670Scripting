6ae3939c0784d1eeff520bdea2461840b78421c9
Niels Dunn <nielsdunn@gmail.com>
2016-12-13 12:21:50 -0700

    PM - Added Flying Enemy
    Added flying enemy polymorphic script

---
+++ Potion Maker/Assets/Scripts/Battle Scripts/FlyingEnemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingEnemy : Enemy {
+
+	void Start()
+	{
+		StartCoroutine (Move(leftPosition));
+	}
+
+	IEnumerator Move(Vector3 target)
+	{
+		while (Mathf.Abs((target - transform.localPosition).x) > 0.20f) 
+		{
+			Vector3 direction = target.x == rightPosition.x ? Vector3.right : Vector3.left;
+			transform.localPosition += direction * Time.deltaTime * speed;
+
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (0.5f);
+
+		Vector3 newTarget = target.x == rightPosition.x ? leftPosition : rightPosition;
+
+		StartCoroutine(Move(newTarget));
+	}
+
+}

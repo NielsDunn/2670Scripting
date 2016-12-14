2c359b1bd50b902d23224b9256c4b34be88b7149
Niels Dunn <nielsdunn@gmail.com>
2016-12-14 13:13:07 -0700

    SC - BGM now persists along all scenes
    It also destroys itself if it already exists.

---
+++ Star Catcher/Assets/GE Files/Scripts/Misc/BGM.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGM : MonoBehaviour {
+
+	public void Awake()
+     {
+         DontDestroyOnLoad(this);
+ 
+         if (FindObjectsOfType(GetType()).Length > 1)
+         {
+             Destroy(gameObject);
+         }
+     } 
+}
